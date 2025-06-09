7fafce90b5d964c8d4f028a6793d9d115798eca0
ditran93 <ditran93@gmail.com>
2025-06-09 10:54:56 -0500

    first commit

---
+++ src/main/java/com/revature/schoolz/SchoolzApplication.java
@@ -0,0 +1,13 @@
+package com.revature.schoolz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/schoolz/SchoolzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.schoolz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
